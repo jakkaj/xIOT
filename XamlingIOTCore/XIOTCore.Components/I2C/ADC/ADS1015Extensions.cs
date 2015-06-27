4bcfc8b21f01995efefa135df8b8391fe25d3123
Jordan Knight <jakkaj@gmail.com>
2015-06-27 12:27:59 +1000

    commiting for JK

---
+++ XamlingIOTCore/XIOTCore.Components/I2C/ADC/ADS1015Extensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitsNet;
+using UnitsNet.Units;
+using XIOTCore.Contract.Enum;
+
+namespace XIOTCore.Components.I2C.ADC
+{
+    public static class ADS1015Extensions
+    {
+        public static ushort ForADS1015(this SamplesPerSecond samples)
+        {
+            ushort[] samplePerSecondMap = { 0x0000, 0x0020, 0x0040, 0x0060, 0x0080, 0x00A0, 0x00C0 };
+            return samplePerSecondMap[(int)samples];
+        }
+
+        public static ushort ForADS1015(this Gain gain)
+        {
+            ushort[] programmableGainMap = { 0x0000, 0x0200, 0x0400, 0x0600, 0x0800, 0x0A00 };
+            return programmableGainMap[(int) gain];
+        }
+
+        public static ElectricPotential ToElectricPotenital(this Gain gain)
+        {
+            switch (gain)
+            {
+                case Gain.Volt5:
+                    return ElectricPotential.From(5, ElectricPotentialUnit.Volt);
+                case Gain.Volt33:
+                    return ElectricPotential.From(3.3, ElectricPotentialUnit.Volt);
+                default:
+                    throw new NotImplementedException($"No ElectricPotential mapping for {gain}");
+            }
+        }
+
+        public static ushort ForADS1015Scale(this ushort gain)
+        {
+            ushort[] programmableGain_Scaler = { 6144, 4096, 2048, 1024, 512, 256 };
+            return programmableGain_Scaler[(int)gain];
+        }
+
+        
+    }
+}
---
+++ XamlingIOTCore/XamlingIOTCore.RaspberryPI/ExplorerHATPro/Components/ExplorerHat_GreenLed.cs
@@ -0,0 +1,13 @@
+﻿using XCore.RaspberryPI.Interface;
+using XIOTCore.Components.Gpio;
+using XIOTCore.Contract.Interface;
+
+namespace XCore.RaspberryPI.ExplorerHATPro.Components
+{
+    public class ExplorerHat_GreenLed : XGpioLed, IExplorerHat_GreenLed
+    {
+        public ExplorerHat_GreenLed(IXGpio gpio) : base(gpio)
+        {
+        }
+    }
+}
