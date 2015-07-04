cb6f03355c7e005db4dfaf78180cda634e923a06
Jordan Knight <jakkaj@gmail.com>
2015-07-04 14:49:47 +1000

    Demos with a sonic distance tester

--- XamlingIOTCore/XIOTCore_Samples/MainPage.xaml.cs
+++ XamlingIOTCore/XIOTCore_Samples/MainPage.xaml.cs
@@ -34,6 +34,9 @@ namespace XIOTCore_Samples
         private readonly IXGpioControl _redLed;
         private readonly IXGpioControl _greenLed;
 
+        private IXGpioControl _output1;
+        private IXGpioControl _input2;
+
         private readonly IExplorerHat_AnaloguePlug _plug1;
         private readonly IExplorerHat_AnaloguePlug _plug4;
 
@@ -52,16 +55,96 @@ namespace XIOTCore_Samples
             _plug4 = _factory.GetComponent<IExplorerHat_AnaloguePlug4>();
 
             _input1 = _factory.GetComponent<IExplorerHat_Input1>();
-            
+
+            _input2 = _factory.GetComponent<IExplorerHat_Input2>();
+            _output1 = _factory.GetComponent<IExplorerHat_Output1>();
+
             _cycle();
+            _cycle2();
         }
 
-        async void _cycle()
+        private async void _cycle()
         {
             //var i2cinfo = _factory.GetComponent<IXI2CInfo>();
             //await i2cinfo.GetAllDevices();
 
+            _output1.On();
 
+            await Task.Delay(1000);
+
+            //return;
+
+            var averages = new List<decimal>();
+
+            while (true)
+            {
+                await Task.Delay(20);
+
+                _output1.Off();
+                await Task.Delay(20);
+                _output1.On();
+
+                var sw = new Stopwatch();
+
+                var sw2 = new Stopwatch();
+
+                sw2.Start();
+                while (!_input2.State)
+                {
+                    if (sw2.Elapsed.TotalSeconds > 1)
+                    {
+                        break;
+                    }
+                }
+
+                sw.Start();
+
+                sw2 = new Stopwatch();
+                sw2.Start();
+                while (_input2.State)
+                {
+                    if (sw2.Elapsed.TotalSeconds > 1)
+                    {
+                        break;
+                    }
+                }
+
+                sw.Stop();
+
+                var freq = Stopwatch.Frequency;
+
+                var ts = sw.ElapsedTicks;
+
+                ts = ts / 2;
+
+                Int64 sos = 344000;
+
+                decimal result = ((1M / freq) * ts) * sos;
+
+                if (result > 4000)
+                {
+                    continue;
+                }
+
+                averages.Add(result);
+
+                while (averages.Count > 10)
+                {
+                    averages.RemoveAt(1);
+                }
+
+                var averageResult = averages.Average();
+
+                var averageRound = Math.Round(averageResult, 0);
+
+                DistanceText.Text = averageRound.ToString();
+                _moveDistanceEllipse(Convert.ToInt32(averageRound));
+            }
+
+        }
+
+        async void _cycle2()
+        {
             var init4 = await _plug4.Init();
             var init1 = await _plug1.Init();
 
@@ -80,40 +163,84 @@ namespace XIOTCore_Samples
                 // state = !state;
 
 
-                var v1 = await _plug1.MeasurePercentage();
-                var v4 = await _plug4.MeasurePercentage();
+                var v1 = await _plug4.MeasurePercentage();
 
-                if (v1 > v4 + 5)
-                {
-                    _greenLed.State = false;
-                    _redLed.State = true;
-                }
-                else if (v4 > v1 + 5)
-                {
-                    _greenLed.State = true;
-                    _redLed.State = false;
-                }
-                else
-                {
-                    _greenLed.State = true;
-                    _redLed.State = true;
-                }
+                _moveVerticalEllipse(v1);
+
+                //var v4 = await _plug4.MeasurePercentage();
+
+                //if (v1 > v4 + 5)
+                //{
+                //    _greenLed.State = false;
+                //    _redLed.State = true;
+                //}
+                //else if (v4 > v1 + 5)
+                //{
+                //    _greenLed.State = true;
+                //    _redLed.State = false;
+                //}
+                //else
+                //{
+                //    _greenLed.State = true;
+                //    _redLed.State = true;
+                //}
 
                 //Debug.WriteLine($"Plug 1: {v1}, Plug 4: {v4}");
 
-                _moveEllipse(v1, v4);
+               // _moveEllipse(v1, v4);
 
                 var state = _input1.State;
-                
+
                 Debug.WriteLine($"IR State: {state}");
 
                 MovementText.Visibility = state ? Visibility.Visible : Visibility.Collapsed;
 
-                await Task.Yield();
+                await Task.Delay(20);
             }
         }
 
         private double _lastPos = 0;
+        private double _distanceLastPos = 0;
+        private double _lightLastPos = 0;
+        void _moveDistanceEllipse(int amount)
+        {
+            var screenWidth = Window.Current.Bounds.Width;
+
+            var absoluteWidth = (1D / 1500) * amount;
+
+            var pos = screenWidth * absoluteWidth;
+
+
+            if (_distanceLastPos < amount + 10 && _distanceLastPos > amount - 10)
+            {
+                return;
+            }
+
+            _distanceLastPos = amount;
+
+            DistanceCompositeTransform.TranslateX = -pos;
+        }
+
+        void _moveVerticalEllipse(double percentage)
+        {
+            var screenHeight = Window.Current.Bounds.Height;
+
+            var abs = 1/percentage;
+
+            var offsetHeight = screenHeight*abs;
+
+            double pos = offsetHeight * 5;
+            
+            //remove some jitter from the measurements. 
+            if (_lightLastPos < percentage + 1 && _lightLastPos > percentage - 1)
+            {
+                return;
+            }
+
+            _lightLastPos = percentage;
+
+            ThingCompositeTransform.TranslateY = -pos;
+        }
 
         void _moveEllipse(double left, double right)
         {
