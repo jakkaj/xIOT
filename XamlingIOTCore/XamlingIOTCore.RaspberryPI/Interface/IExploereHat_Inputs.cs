767dd5da53b5cd6f2c0668ce862fa86b2d7a7400
Jordan Knight <jakkaj@gmail.com>
2015-07-11 15:30:18 +1000

    Missing files

---
+++ XamlingIOTCore/XamlingIOTCore.RaspberryPI/Interface/IExploereHat_Inputs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XIOTCore.Contract.Interface;
+
+namespace XCore.RaspberryPI.Interface
+{
+    public interface IExplorerHat_Input1 : IXGpioControl
+    {
+    }
+    public interface IExplorerHat_Input2 : IXGpioControl
+    {
+    }
+    public interface IExplorerHat_Input3 : IXGpioControl
+    {
+    }
+    public interface IExplorerHat_Input4 : IXGpioControl
+    {
+    }
+}
