b6311accd6a85c9ec8c4096f27c9abe5c5ce8e4a
Jordan Knight <jakkaj@gmail.com>
2015-07-19 18:09:17 +1000

    Added back in missing files that my merge stripped.

---
+++ Samples/HC-SR04/xIOT_Sample_HCSR04/xIOT_Sample_HCSR04/MainPage.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Autofac;
+using XCore.RaspberryPI.Interface;
+using XIOTCore.Components.Modules.Range;
+using XIOTCore.Contract;
+using XIOTCore.Contract.Interface;
+using XIOTCore.Contract.Interface.Module;
+using XIOTCore.Factory;
+
+//For the wiring diagram see: http://fritzing.org/projects/hc-sr04-echo-range-finder-and-xiot-windows
+//License and other samples / nuget package source: https://github.com/jakkaj/Xamling-IOT
+//This sample doco: https://github.com/jakkaj/Xamling-IOT/tree/master/Samples/HC-SR04
+//Jordan Knight @jakkaj / Xaming 2015. 
+
+
+namespace xIOT_Sample_HCSR04
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private readonly IXIOTCoreFactory _factory =
+            XIOTCoreFactory.Create(Platforms.RaspberryPi2ModelB | Platforms.RaspberryPi2ExporerHatPro);
+
+        private IHC_SR04 _echoLocationModule;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+
+
+            //Uncomment out this sample for straight, non MVVM, non dependency injection style
+            /*
+
+            _factory.Init();
+            
+
+            var input2 = _factory.GetComponent<IExplorerHat_Input2>(); //Get the digital input 2 from the Explorer HAT
+            var output1 = _factory.GetComponent<IExplorerHat_Output1>(); //Get the digital output 1 from the Explorer HAT. 
+
+            _echoLocationModule = new HC_SR04(output1, input2);
+
+            _cycle();
+            */
+
+
+            //The better, MVVM/Autofac way. 
+            
+            _factory.Builder.RegisterType<MainViewModel>().AsSelf(); //register the vm for Autofac resolution
+
+            //Do the tricky configs here in a central location, then you can inject it in to all of your classes 
+            //with minimal fuss!
+
+            _factory.Builder.Register(
+                c => new HC_SR04(c.Resolve<IExplorerHat_Output1>(), c.Resolve<IExplorerHat_Input2>())).As<IHC_SR04>();
+
+            _factory.Init();
+
+            var vm = _factory.Container.Resolve<MainViewModel>();
+
+            DataContext = vm;
+        }
+
+        private async void _cycle()
+        {
+            await _echoLocationModule.Init();
+
+            while (true)
+            {
+                var averageRound = await _echoLocationModule.Measure(true);
+
+                DistanceText.Text = averageRound.ToString();
+               
+            }
+        }
+    }
+}
---
+++ Samples/HC-SR04/xIOT_Sample_HCSR04/xIOT_Sample_HCSR04/MainViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using xIOT_Sample_HCSR04.Annotations;
+using XCore.RaspberryPI.Interface;
+using XIOTCore.Contract.Interface.Module;
+
+//For the wiring diagram see: http://fritzing.org/projects/hc-sr04-echo-range-finder-and-xiot-windows
+//License and other samples / nuget package source: https://github.com/jakkaj/Xamling-IOT
+//This sample doco: https://github.com/jakkaj/Xamling-IOT/tree/master/Samples/HC-SR04
+//Jordan Knight @jakkaj / Xaming 2015. 
+
+namespace xIOT_Sample_HCSR04
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private IHC_SR04 _echoLocationModule;
+
+        private string _distance;
+
+        public MainViewModel(IHC_SR04 echoLocationModule)
+        {
+            _echoLocationModule = echoLocationModule;
+            _init();
+        }
+
+        async void _init()
+        {
+            await _echoLocationModule.Init();
+            _loop();
+        }
+
+        private async void _loop()
+        {
+            while (true)
+            {
+                var averageRound = await _echoLocationModule.Measure(true);
+                Distance = averageRound.ToString();
+                await Task.Yield(); //make sure other stuff can do things. 
+            }
+        }
+
+        public string Distance
+        {
+            get { return _distance; }
+            set
+            {
+                _distance = value;
+                OnPropertyChanged();
+            }
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
