caf7bf891cf9ef9bd95900b1b14a32797f3325bb
Jordan Knight <jakkaj@gmail.com>
2015-07-26 15:21:18 +1000

    Started LCD sample code

---
+++ Samples/LCD-Hitatchi-HD44780/LCD_Sample_Console/LcdExamples_FTDI.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+using XIOTCore.Contract;
+using XIOTCore.Contract.Enum;
+using XIOTCore.Contract.Interface;
+using XIOTCore.Contract.Interface.Basics;
+using XIOTCore.Contract.Interface.Module;
+using XIOTCore.Portable.Components.LCD.HD44780;
+using XIOTCore.Portable.Factory;
+using XIOTCore.Portable.Util.XamlingCore;
+
+namespace XIOTCore_Samples_Console.LCD
+{
+    public class LcdExamples_FTDI
+    {
+        private readonly IXIOTCoreFactory _factory =
+          XIOTCoreFactory.Create(Platforms.FTDI_USB);
+
+        private II2CLCD _lcd;
+
+      
+        public async Task Init()
+        {
+            _factory.Init();
+
+            var i2c = _factory.GetComponent<IXI2CDevice>();
+
+            _lcd = new I2CLCD(i2c, 0x27, 2, 1, 0, 4, 5, 6, 7, 3, BacklightPolarity.Positive);
+
+            await _lcd.Begin(16,2);
+
+            for (int i = 0; i < 3; i++)
+            {
+                _lcd.BackLight();
+                StopwatchDelay.Delay(100);
+                _lcd.NoBacklight();
+                StopwatchDelay.Delay(100);
+            }
+
+            _lcd.BackLight();
+            _lcd.Home();
+            _lcd.SetCursor(0, 0); //Start at character 4 on line 0
+            _lcd.Write("FT232H, LCD, C#");
+            StopwatchDelay.Delay(250);
+            _lcd.SetCursor(0, 1);
+            _lcd.Write("git.io/vmEdE");
+        }
+    }
+}
---
+++ Samples/LCD-Hitatchi-HD44780/LCD_Sample_Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCD_Sample_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sampleLCD = new LCD.LcdExamples_FTDI();
+
+            var t = sampleLCD.Init();
+
+            t.Wait();
+        }
+    }
+}
