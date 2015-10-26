6e5427e31d5e84c0da5bb042e8c44555fd99ee03
Jordan Knight <jakkaj@gmail.com>
2015-10-26 12:27:26 +1100

    Updated the samples for OLED to Windows UWP RTM.

---
+++ Samples/OLED_SSD1306/OLED_Sample_Universal_RTM/MainPage.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+using XIOTCore.Contract;
+using XIOTCore.Contract.Enum;
+using XIOTCore.Contract.Interface;
+using XIOTCore.Contract.Interface.Devices;
+using XIOTCore.Universal.Factory;
+using XIOTCore.Universal.Windows.Util;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace OLED_Sample_Universal_RTM
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private readonly IXIOTCoreFactory _factory =
+           XIOTCoreWindowsFactory.Create(Platforms.RaspberryPi2ModelB | Platforms.RaspberryPi2ExporerHatPro);
+        public MainPage()
+        {
+            this.InitializeComponent();
+            _init();
+        }
+
+        async void _init()
+        {
+            await Task.Delay(500);
+            
+            _factory.Init();
+            var oled = _factory.GetComponent<IOLED_SSD1306_I2C>();
+
+            await oled.Init(OLEDConstants.SSD1306_I2C_ADDRESS, OLEDDisplaySize.SSD1306_128_64);
+
+            oled.Display();
+
+            while (true)
+            {
+                var r = new RenderTargetBitmap();
+
+                await r.RenderAsync(OLEDRenderer);
+
+                var colors = await PixelRender.GetPixels(r);
+
+                for (var i = 0; i < r.PixelHeight; i++)
+                {
+                    for (var x = 0; x < r.PixelWidth; x++)
+                    {
+                        var pixel = colors[x, i];
+                        var average = (pixel.Red + pixel.Green + pixel.Blue) / 3;
+                        if (average > 0)
+                        {
+                            oled.DrawPixel((ushort)x, (ushort)i, 1);
+                        }
+                        else
+                        {
+                            oled.DrawPixel((ushort)x, (ushort)i, 0);
+                        }
+                    }
+                }
+                oled.Display();
+                await Task.Yield();
+            }
+        }
+    }
+}
---
+++ Samples/OLED_SSD1306/OLED_Sample_Universal_RTM/View/OLEDRenderView.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace OLED_Sample_Universal_RTM.View
+{
+    public sealed partial class OLEDRenderView : UserControl
+    {
+        public OLEDRenderView()
+        {
+            this.InitializeComponent();
+            _wait2();
+            _wait();
+        }
+
+        async void _wait2()
+        {
+            while (true)
+            {
+                VisualStateManager.GoToState(this, "Upped", true);
+                await Task.Delay(4000);
+                VisualStateManager.GoToState(this, "Downed", true);
+                await Task.Delay(4000);
+            }
+        }
+
+        async void _wait()
+        {
+            while (true)
+            {
+                await Task.Delay(1000);
+                TheText.Text = DateTime.Now.ToString("HH:mm:ss tt");
+            }
+        }
+    
+}
+}
